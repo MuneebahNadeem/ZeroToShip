c9eabd0aaade0b4878285fb78d105b0bdff6dd02
MuneebahNadeem <muneebah.3243@gmail.com>
2026-07-22 15:03:14 +0500

    Phase 1 Complete: Establish Django database schemas, relationship constraints along with manual testing

--- Phase-1/core/models.py
+++ Phase-1/core/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    MEMBERSHIP_CHOICES = [
+        ('Student', 'Student'),
+        ('Admin', 'Admin'),
+    ]
+
+    member_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 100)
+    email = models.EmailField(unique = True)
+    password_hash = models.CharField(max_length=255)
+    membership_type = models.CharField(
+        max_length = 20,
+        choices = MEMBERSHIP_CHOICES,
+    )
+
+    def __str__(self):
+        return self.name
+
+class SportsClub(models.Model):
+    club_id = models.AutoField(primary_key=True)
+    club_name = models.CharField(max_length=100)
+    coach_name = models.CharField(max_length=100)
+    max_capacity_students = models.IntegerField()
+
+    def __str__(self):
+        return self.club_name
+
+class Roster(models.Model):
+    roster_id = models.AutoField(primary_key=True)
+
+    member = models.ForeignKey(
+        Member,
+        on_delete = models.CASCADE,
+    )
+
+    club = models.ForeignKey(
+        SportsClub,
+        on_delete = models.CASCADE,
+    )
+
+    join_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.member.name} - {self.club.club_name}"
