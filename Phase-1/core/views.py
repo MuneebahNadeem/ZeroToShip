d821f932f99e550f6c0bec2c766320c7c10c58d3
MuneebahNadeem <muneebah.3243@gmail.com>
2026-07-26 16:54:07 +0500

    Phase 2: Implement User Registration endpoint

--- Phase-1/core/views.py
+++ Phase-1/core/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import json
 
+from .models import Member
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 # Create your views here.
+def register(request):
+    data = json.loads(request.body)
+
+    name = data.get("name")
+    email = data.get("email")
+    password = data.get("password")
+    membership_type = data.get("membership_type")
+
+    if not name or not email or not password or not membership_type: 
+        return HttpResponse("Missing required fields!")
+
+    if Member.objects.filter(email=email).exists():
+        return HttpResponse("Email already exists!")
+
+    password_hash = make_password(password)
+
+    member = Member.objects.create(
+        name=name,
+        email=email,
+        password_hash=password_hash,
+        membership_type=membership_type
+    )
+
+    return HttpResponse("Registration Succesful!")
+
+def login(request):
+    return HttpResponse("Login Endpoint")
--- Phase-1/sportsclub/urls.py
+++ Phase-1/sportsclub/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
